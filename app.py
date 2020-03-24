7df3bf7454778501205352328751db6e9e785c5f
George Schena <george@madetech.com>
2020-03-24 21:41:15 +0000

    Cleaned data fully, and implement DB

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, escape, request
+from flask_sqlalchemy import SQLAlchemy
 import re
 import itertools
 
@@ -9,6 +10,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://georgeschena:password@localhost/property_scraper'
+
+
+class Properties(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(255), nullable=True)
+    refernce_number = db.Column(db.String(60),  nullable=True)
+    received_date = db.Column(db.String(60),  nullable=True)
+    validated_date = db.Column(db.String(60),  nullable=True)
+    status = db.Column(db.String(60),  nullable=True)
 
 
 @app.route('/')
@@ -46,30 +60,46 @@ def hello():
     cleaned_address = []
     for x in address:
         clean = re.sub(r'\>(.*?)\<', ' ', str(x))
-        cleaner = clean.replace("\n", "")
-        cleaned_address.append(cleaner)
+        remove_class = clean.replace(r'<p class="address">', "")
+        remove_tags = remove_class.replace(r'</p>', "")
+        strip_spaces = remove_tags.strip()
+        cleaned_address.append(strip_spaces)
 
     refernce_numbers = []
     for x in cleaned_metainfo:
         clean = re.search(r'(?=Ref. No:).*(?=Received)', str(x)).group(0)
-        refernce_numbers.append(clean)
+        remove_class = clean.replace(r'<span class="divider" /span> ', "")
+        remove_ref_string = remove_class.replace(r'Ref. No:', "")
+        strip_spaces = remove_ref_string.strip()
+        refernce_numbers.append(strip_spaces)
 
     received_dates = []
     for x in cleaned_metainfo:
         clean = re.search(r'(?=Received:).*(?=Validated:)', str(x)).group(0)
-        received_dates.append(clean)
+        remove_class = clean.replace(r'<span class="divider" /span> ', "")
+        remove_received_string = remove_class.replace(r'Received:', "")
+        strip_spaces = remove_received_string.strip()
+        received_dates.append(strip_spaces)
 
     validated_dates = []
     for x in cleaned_metainfo:
         clean = re.search(r'(?=Validated:).*(?=Status:)', str(x)).group(0)
-        validated_dates.append(clean)
+        remove_class = clean.replace(r'<span class="divider" /span> ', "")
+        remove_validated_string = remove_class.replace(r'Validated:', "")
+        strip_spaces = remove_validated_string.strip()
+        validated_dates.append(strip_spaces)
 
     statuses = []
     for x in cleaned_metainfo:
         clean = re.search(r'(?=Status:).*(?=)', str(x)).group(0)
-        statuses.append(clean)
+        remove_tags = clean.replace(r'</p>', "")
+        remove_status_string = remove_tags.replace(r'Status:', "")
+        strip_spaces = remove_status_string.strip()
+        statuses.append(strip_spaces)
 
-    for (a, b, c, d, e) in itertools.izip_longest(cleaned_address, refernce_numbers, received_dates, validated_dates, statuses):
-        print(a, b, c, d, e)
+    for a in statuses:
+        prop = Properties(address=a)
+        db.session.add(prop)
+        db.session.commit()
 
     return str(statuses)
