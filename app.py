6185486ee2b9d7897f173873eed5aea1f3cfb319
George Schena <george@madetech.com>
2020-03-31 18:28:48 +0100

    Add endpoint for only todays properties

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, escape, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from property_model import Property
 import os
+from datetime import date
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -16,4 +17,22 @@ def get_all_properties():
         {
             "count": Property.query.count(),
             "properties": list(map(lambda prop: prop.serialize(), Property.query.all()))
+        })
+
+
+@app.route('/todays_properties')
+def get_todays_properties():
+    today = date.today()
+    formatted_todays_date = today.strftime("%a %d %b %Y")
+
+    properties_today = Property.query.filter_by(
+        received_date=formatted_todays_date).all()
+
+    properties_today_count = Property.query.filter_by(
+        received_date=formatted_todays_date).count()
+
+    return jsonify(
+        {
+            "count": properties_today_count,
+            "properties": list(map(lambda prop: prop.serialize(), properties_today))
         })
